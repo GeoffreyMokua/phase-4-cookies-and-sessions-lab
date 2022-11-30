f9aade790ec436af3594ac0f9ae5d6dceacc69d5
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-11-30 21:11:55 +0300

    completed assignment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,25 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
   def index
     articles = Article.all.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    # article = Article.find(params[:id])
+    # render json: article
+    session[:page_views] ||= 0
+    if session[:page_views] < 3
+      article = Article.find(params[:id])
+      session[:page_views] += 1
+      render json: article
+    else
+      session[:page_views] += 1 
+      render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
+    end
   end
 
   private
-
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
   end
